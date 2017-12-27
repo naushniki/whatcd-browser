888f0aaa9028bc25246034c0011acab9dd34c1cb
Vitaly Pryakhin <v.pryakhin@gmail.com>
2017-12-27 05:37:41 +0300

    make similarity graph visualization performance more adequate

--- app/models/artist_group.rb
+++ app/models/artist_group.rb
@@ -22,38 +22,87 @@ class ArtistGroup < ActiveRecord::Base
       where ar.artistid=#{self.ArtistID.to_s}")
   end
 
-  def construct_similarity_graph_for_given_level_of_separation(level)
-    nodes = self.similar
+  def load_top_similar_artists(limit=120)
+    ArtistGroup.find_by_sql("
+      select ag.*
+      from artists_similar ar      
+      join artists_similar ar2 on (ar.SimilarID=ar2.SimilarID and ar2.artistid!=ar.artistid)
+      join artists_group ag on ag.artistid=ar2.artistid
+      join artists_similar_scores sc on sc.SimilarID=ar.SimilarID      
+      where ar.artistid=#{self.ArtistID.to_s}
+      order by sc.score desc limit #{limit};")
+  end
+
+  #level - level of separation between artist and loaded similar artists.
+  #limit - maximum amount of artists to be added on each level of separarion above 1. If limit value is greater,
+  #than the existing number of artists for given level of separation, the ones with the lower scores get excluded.
+  def construct_similarity_graph_for_given_level_of_separation(level, limit = 45)
+    nodes = self.load_top_similar_artists()
     edges = Array.new
     nodes.each do |node|
-      edges.push({"from" => self.ArtistID, "to" => node.ArtistID})
+      vals = [self.ArtistID, node.ArtistID].sort
+      edge = {"from" => vals[0], "to" => vals[1]}
+      edges.push(edge)
     end
     self.instance_variable_set(:@similar_artists_loaded, true)
     nodes.push(self)
     i = 1
-    while i <= level      
-      nodes.clone.each do |node|
-        next if defined? node.similar_artists_loaded
-        new_nodes = node.similar
-        new_nodes.each do |new_node|
-          if !nodes.include?(new_node) and i < level
-            nodes.push(new_node)
-          end
-          vals = [node.ArtistID, new_node.ArtistID].sort
-          edge = {"from" => vals[0], "to" => vals[1]}
-          if !edges.include?(edge)
-            edges.push(edge)
-          end
-        end
+    while i < level
+      new_ids = nodes.map { |x| if !(x.instance_variable_get(:@similar_artists_loaded)==true) then x.ArtistID else nil end}.compact
+      all_ids = nodes.map { |x| x.ArtistID }.compact
+      data = load_connections(new_ids, all_ids, limit)
+      nodes.each do |node|
         node.instance_variable_set(:@similar_artists_loaded, true)
       end
+      ids_to_load = data.map { |entry| 
+        if (!nodes.include?(ArtistGroup.new({:ArtistID => entry["ArtistID2"]})))
+          entry["ArtistID2"]
+        else nil end
+        }.compact
+      nodes += ArtistGroup.find(ids_to_load)
+      data.each do |entry|
+        vals = [entry["ArtistID1"], entry["ArtistID2"]].sort
+        edge = {"from" => vals[0], "to" => vals[1]}  
+        if (!edges.include?(edge) and nodes.include?(ArtistGroup.new({:ArtistID => entry["ArtistID2"]})) and nodes.include?(ArtistGroup.new({:ArtistID => entry["ArtistID1"]})))
+          edges.push(edge)
+        end
+      end      
       i += 1
     end
     return nodes, edges
+  end
+
+  def load_connections(new_ids, all_ids, limit)    
+    query = "
+      select ar.artistid, ar2.artistid, sc.score
+      from artists_similar ar      
+      join artists_similar ar2 on (ar.SimilarID=ar2.SimilarID and ar2.artistid!=ar.artistid)
+      join artists_group ag on ag.artistid=ar2.artistid
+      join artists_similar_scores sc on sc.SimilarID=ar.SimilarID
+      where ar.artistid in (#{new_ids.join(',')})
+      order by sc.score desc limit #{limit};"   
+      new_connections = ActiveRecord::Base.connection.execute(query)
+                        .map {|x| {"ArtistID1" => x[0], "ArtistID2" => x[1], "score" => x[2]}}
+    query = "
+      select ar.artistid, ar2.artistid, sc.score
+      from artists_similar ar      
+      join artists_similar ar2 on (ar.SimilarID=ar2.SimilarID and ar2.artistid!=ar.artistid)
+      join artists_group ag on ag.artistid=ar2.artistid
+      join artists_similar_scores sc on sc.SimilarID=ar.SimilarID
+      where ar.artistid in (#{new_ids.join(',')})
+      and (ar2.artistid in (#{all_ids.join(',')}) 
+      or ar2.artistid in (#{new_ids.join(',')}) 
+      or ar2.artistid in (#{new_connections.map { |x| x["ArtistID2"] }.join(',')}))
+      order by sc.score desc;"
+      conections_to_existing_nodes = ActiveRecord::Base.connection.execute(query)
+                        .map {|x| {"ArtistID1" => x[0], "ArtistID2" => x[1], "score" => x[2]}}
+      return new_connections + conections_to_existing_nodes
   end
 
   def self.random
     total_rows = 885304 # this will never change
     ArtistGroup.offset(rand(total_rows)).limit(1).first
   end
+
+  private :load_connections
 end
